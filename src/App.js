59fbe3fb7970c21c210954fcb1922f1b4f6c0312
Brian Duong <brianminhduong@gmail.com>
2022-07-27 19:39:56 -0400

    Implement dispatch with showNotification reducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-
-import { useSelector } from "react-redux/es/exports";
+import { uiActions } from "./store";
 
 function App() {
+  const dispatch = useDispatch();
+
   const show = useSelector((state) => state.ui.show);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch("https://react-http-6e23b-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-6e23b-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!!",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit/";
 
-const initialState = { show: false };
+const initialState = { show: false, notification: null };
 
 const uiSlice = createSlice({
   name: "ui",
@@ -8,6 +8,13 @@ const uiSlice = createSlice({
   reducers: {
     showCart(state) {
       state.show = !state.show;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
