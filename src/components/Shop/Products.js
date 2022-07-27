9ec08348949b5835d2ad55043065186733d9e2c5
Brian Duong <brianminhduong@gmail.com>
2022-07-26 22:16:19 -0400

    Create dummy products for Products component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,17 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "First Book I ever Wrote",
+  },
+  {
+    id: "p2",
+    price: 9,
+    title: "My Second Book",
+    description: "Second Book I ever Wrote",
+  },
+];
 
 const Products = (props) => {
+  const productItemsArray = DUMMY_PRODUCTS.map((product) => {
+    return (
+      <ProductItem
+        key={product.id}
+        title={product.title}
+        price={product.price}
+        description={product.description}
+      />
+    );
+  });
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
+      <ul>{productItemsArray}</ul>
     </section>
   );
 };
