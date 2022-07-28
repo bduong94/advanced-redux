222bd0a88acd67e6f8b47665b12b6cb688f98b95
Brian Duong <brianminhduong@gmail.com>
2022-07-27 20:11:49 -0400

    Refactor HTTP request to PUTcart data in cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit/";
 
+import { uiActions } from "./ui-slice";
+
 const initialState = {
   items: [],
   totalQuantity: 0,
@@ -41,5 +43,45 @@ const cartSlice = createSlice({
     },
   },
 });
+
+const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-6e23b-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    }
+
+    dispatch();
+  };
+};
 
 export default cartSlice;
